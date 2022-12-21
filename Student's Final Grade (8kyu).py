a9f8c3a2c40ed9a4c0863c2f440d7783c710820d
Adik13 <Maslennikoff88@gmail.com>
2022-12-21 11:42:33 +0300

    my task solution

--- Student's Final Grade (8kyu).py
+++ Student's Final Grade (8kyu).py
@@ -17,3 +17,12 @@
 # 75, if a grade for the exam is more than 50
 # and if a number of completed projects is minimum 2.
 # 0, in other cases
+def final_grade(exam, projects):
+    if exam > 90 or projects > 10:
+        return 100
+    elif exam > 75 and projects >= 5:
+        return 90
+    elif exam > 50 and projects >= 2:
+        return 75
+    else:
+        return 0
